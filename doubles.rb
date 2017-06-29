b9671e17798fba24be0ea06d05cd819583c1f48a
naderae <naderabouezze93@gmail.com>
2017-06-28 21:45:29 -0400

    just finished doubles

--- doubles.rb
+++ doubles.rb
@@ -1,6 +1,14 @@
+
+
 roll_1 = 1 + Random.rand(6)
 roll_2 = 1 + Random.rand(6)
 sum = roll_1 + roll_2
 
-puts "you rolled #{roll_1} and #{roll_2}"
-puts "your total is #{sum}"
+if roll_1 == roll_2
+  puts "you rolled #{roll_1} and #{roll_2}"
+  puts "Doubles!"
+  puts "your total is #{sum}"
+else
+  puts "you rolled #{roll_1} and #{roll_2}"
+  puts "your total is #{sum}"
+end
